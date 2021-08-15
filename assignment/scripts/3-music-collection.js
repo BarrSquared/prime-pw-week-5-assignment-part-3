6cbc9c3a9f201d90d947a8d7188c99b24c770d84
Russell Barr <rbarrsquared@gmail.com>
2021-08-15 11:29:49 -0500

    w5, p3, standard goals

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,30 +1,30 @@
 console.log('***** Music Collection *****')
-//comment; collection var, empty []
-
+//collection var, empty []
 let collection = [];
 
-//comment; collection var, create obj w/ album properties
-
+// collection var, create obj w/ album properties
 function addToCollection (title, artist, yearPublished) {
   let albumInfo = {
-    title: title,
-    artist: artist,
-    yearPublished: yearPublished
+    'title': title,
+    'artist': artist,
+    'yearPublished': yearPublished
   }
   collection.push(albumInfo);
   return collection;
 }
 
-//comment;test for addToCollection, adding info then console.logging it
+//test for addToCollection, adding info then console.logging it
 addToCollection ('testTitle1', 'testArtist1', 'testYearPub1');
 addToCollection ('testTitle2', 'testArtist2', 'testYearPub2');
-addToCollection ('testTitle3', 'testArtist3', 'testYearPub3');
+addToCollection ('testTitle3', 'testArtist1', 'testYearPub3');
 addToCollection ('testTitle4', 'testArtist4', 'testYearPub4');
-addToCollection ('testTitle5', 'testArtist5', 'testYearPub5');
+addToCollection ('testTitle5', 'testArtist1', 'testYearPub5');
 addToCollection ('testTitle6', 'testArtist6', 'testYearPub6');
 console.log(collection);
 
-//comment; create function showCollection, take in an array, log number of items in array, loop over array and console.log info, T/A/YP
+//test of showCollection
+console.log('Test of collection array and applied functions');
+//create function showCollection, take in an array, log number of items in array, loop over array and console.log info, T/A/YP
 function showCollection (inputArray) {
   console.log('The number of items in this array is: ' + inputArray.length);
   for(let i = 0; i < inputArray.length; i++){
@@ -32,5 +32,26 @@ function showCollection (inputArray) {
   }
   return;
 }
-//comment; test of showCollection
+
+//test of showCollection
 showCollection (collection);
+
+//create findByArtist function, takes in string(artist),loop over collection, return array with matching results
+
+function findByArtist (artist) {
+  //array to hold results
+  let matchingArtist = [];
+  //loop over
+  for(let i = 0; i < collection.length; i++){
+    if(collection[i]['artist'] === artist){
+          //push matches to
+      matchingArtist.push(collection[i]);
+    }
+  }
+  //return array
+  return matchingArtist;
+}
+
+//test for findByArtist
+console.log('Test of findByArtist function. Should list 3 albums by testArtist1.')
+findByArtist('testArtist1');
