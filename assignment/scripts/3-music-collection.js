83241fb6949543a98efa8d37b36f649004cadb8d
Russell Barr <rbarrsquared@gmail.com>
2021-08-15 04:49:39 -0500

    w5, p3, collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,16 @@
 console.log('***** Music Collection *****')
+//comment; collection var, empty []
+
+let collection = [];
+
+//comment; collection var, create obj w/ album properties
+
+function addToCollection (title, artist, yearPublished) {
+  let albumInfo = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  }
+  collection.push(albumInfo);
+  return collection;
+}
