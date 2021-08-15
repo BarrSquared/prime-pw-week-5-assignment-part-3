b9e57a34a99e117d7b357a61a1ec29a50391bb2c
Russell Barr <rbarrsquared@gmail.com>
2021-08-15 10:23:15 -0500

    w5, p3, to showCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -14,3 +14,23 @@ function addToCollection (title, artist, yearPublished) {
   collection.push(albumInfo);
   return collection;
 }
+
+//comment;test for addToCollection, adding info then console.logging it
+addToCollection ('testTitle1', 'testArtist1', 'testYearPub1');
+addToCollection ('testTitle2', 'testArtist2', 'testYearPub2');
+addToCollection ('testTitle3', 'testArtist3', 'testYearPub3');
+addToCollection ('testTitle4', 'testArtist4', 'testYearPub4');
+addToCollection ('testTitle5', 'testArtist5', 'testYearPub5');
+addToCollection ('testTitle6', 'testArtist6', 'testYearPub6');
+console.log(collection);
+
+//comment; create function showCollection, take in an array, log number of items in array, loop over array and console.log info, T/A/YP
+function showCollection (inputArray) {
+  console.log('The number of items in this array is: ' + inputArray.length);
+  for(let i = 0; i < inputArray.length; i++){
+    console.log('TITLE: ' + inputArray[i]['title'] + ' by ' + inputArray[i]['artist'] + ', published in ' + inputArray[i]['yearPublished']);
+  }
+  return;
+}
+//comment; test of showCollection
+showCollection (collection);
